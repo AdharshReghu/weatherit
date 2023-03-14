b89b7cc090303000b980e2b37c3848224bbfb594
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-03-14 17:37:54 +0530

    initial commit

---
+++ lib/screens/loading screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //get current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
